078df5fda4953d522c11cbdc745920e55999f0c6
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-05-21 13:38:35 +0200

    chore: delete discord + twitter

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,14 +96,6 @@ const config = {
                 label: 'Stack Overflow',
                 href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
             ],
           },
           {
