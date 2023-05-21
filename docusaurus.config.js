302650e85566261941018dd17a9d9d0ba43308fd
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-05-21 12:10:41 +0200

    chore: remove buttons

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'BPT DEVELOPMENT',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Programming has never been easier',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
